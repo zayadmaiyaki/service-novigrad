0f51e16c4c43aca9bcce11faab5fd0e474424188
fati12355 <133292699+fati12355@users.noreply.github.com>
2023-10-23 22:52:33 -0400

    Delete Client.java

--- Client.java
+++
@@ -1,9 +0,0 @@
-
-  public class Client extends Utilisateurs {
-
-        public Client (String username, String password){
-            super (username, password, "client");
-            
-        }
-
-      
