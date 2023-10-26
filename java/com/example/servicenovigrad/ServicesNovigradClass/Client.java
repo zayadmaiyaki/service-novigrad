f835ba1a5c9f4b773dce5fe2d79633a757037a25
Slaaayer420 <129106428+Slaaayer420@users.noreply.github.com>
2023-10-26 01:26:58 -0400

    Delete java/com/example/servicenovigrad directory

--- java/com/example/servicenovigrad/ServicesNovigradClass/Client.java
+++
@@ -1,9 +0,0 @@
-package com.example.servicenovigrad.ServicesNovigradClass;
-
-public class Client extends Utilisateurs {
-
-    public Client (String username, String password){
-        super (username, password, "client");
-            
-    }
-}
