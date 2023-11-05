0a61f04894e5f4bf2bb142ba27d6971968844a7d
ThilloAAG <145293301+ThilloAAG@users.noreply.github.com>
2023-11-05 18:39:19 -0500

    Update Service.java

--- Service.java
+++ Service.java
@@ -2,11 +2,23 @@ import java.sql.Time;
 import java.text.DateFormat;
 
 public class Service {
-    String serviceType ; 
-    Time heureDeTravail;
+    
+    private String serviceType ; 
+    private Time heureDeTravail;
+    private List<String> informationsClientRequises; 
+    private List<String> documentsRequis;
+    
     public Service(String serviceType){
         this.serviceType=serviceType;
     }
+
+     public Service(String serviceType, Time heureDeTravail, List<String> informationsClientRequises, List<String> documentsRequis) {
+        this.serviceType = serviceType;
+        this.heureDeTravail = heureDeTravail;
+        this.informationsClientRequises = informationsClientRequises;
+        this.documentsRequis = documentsRequis;
     
-    
+    }
+
+
 }
