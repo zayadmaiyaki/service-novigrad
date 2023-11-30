7c6c33247b1fef99935e4b9a650c1e55b75b5b6e
Slaaayer420 <129106428+Slaaayer420@users.noreply.github.com>
2023-11-29 23:24:21 -0500

    Update OfferedServicesActivity.java

--- app/src/main/java/com/example/servicenovigrad/OfferedServicesActivity.java
+++ app/src/main/java/com/example/servicenovigrad/OfferedServicesActivity.java
@@ -1,4 +1,4 @@
-package com.example.servicenovigrad;
+package com.example.createservice;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
@@ -18,6 +18,7 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -37,101 +38,93 @@ public class OfferedServicesActivity extends AppCompatActivity {
         adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, offeredServiceNames);
         selectedServicesListView.setAdapter(adapter);
 
-        // Get the branch ID from the intent
         String branchId = getIntent().getStringExtra("BRANCH_ID");
         if (branchId != null) {
             loadOfferedServices(branchId);
         } else {
             Toast.makeText(this, "Branch ID not found.", Toast.LENGTH_LONG).show();
-            finish(); // Close the activity if there is no branch ID
+            finish();
         }
-    }
-
-    private void loadOfferedServices(String branchId) {
-        branchesRef = FirebaseDatabase.getInstance().getReference("branches").child(branchId).child("serviceOfferred");
-        branchesRef.addListenerForSingleValueEvent(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                offeredServiceNames.clear();
-                if (!dataSnapshot.exists()) {
-                    Toast.makeText(OfferedServicesActivity.this, "ServiceOffered node does not exist for this branch.", Toast.LENGTH_LONG).show();
-                    return;
-                }
-
-                for (DataSnapshot serviceSnapshot : dataSnapshot.getChildren()) {
-                    // Extracting the service name directly
-                    String serviceName = serviceSnapshot.child("name").getValue(String.class);
-                    if (serviceName != null && !serviceName.trim().isEmpty()) {
-                        offeredServiceNames.add(serviceName);
-                    } else {
-                        Log.d("OfferedServicesActivity", "Service name is null or empty for key: " + serviceSnapshot.getKey());
-                    }
-                }
-
-                if (offeredServiceNames.isEmpty()) {
-                    Toast.makeText(OfferedServicesActivity.this, "No services offered by this branch.", Toast.LENGTH_LONG).show();
-                } else {
-                    adapter.notifyDataSetChanged();
-                }
-            }
-
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
-                Toast.makeText(OfferedServicesActivity.this, "Failed to load services: " + databaseError.getMessage(), Toast.LENGTH_SHORT).show();
-            }
-        });
 
         selectedServicesListView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
             @Override
             public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
-                // Get the selected service ID
-                String selectedServiceId = offeredServiceNames.get(position); // This should be the ID, not the name
+                // Get the selected service name
+                String selectedServiceName = offeredServiceNames.get(position);
 
-                // Create the dialog
-                AlertDialog.Builder builder = new AlertDialog.Builder(OfferedServicesActivity.this);
-
-                // Inflate the dialog_services layout
-                LayoutInflater inflater = getLayoutInflater();
-                View dialogView = inflater.inflate(R.layout.dialog_services, null);
-
-                // Set the custom layout as the dialog's view
-                builder.setView(dialogView);
-
-                TextView serviceNameTextView = dialogView.findViewById(R.id.ServiceNameDialogText);
-                ListView formFieldsListView = dialogView.findViewById(R.id.FormFieldDialogListView);
-                ListView docsFieldsListView = dialogView.findViewById(R.id.DocsFieldDialogListView);
-
-                // Retrieve the service details from the database
-                DatabaseReference serviceRef = FirebaseDatabase.getInstance().getReference("services").child(selectedServiceId);
-                serviceRef.addListenerForSingleValueEvent(new ValueEventListener() {
+                // Fetch the details for the selected service from Firebase
+                DatabaseReference serviceRef = FirebaseDatabase.getInstance().getReference("services");
+                serviceRef.orderByChild("name").equalTo(selectedServiceName).addListenerForSingleValueEvent(new ValueEventListener() {
                     @Override
                     public void onDataChange(DataSnapshot dataSnapshot) {
-                        Service service = dataSnapshot.getValue(Service.class);
-                        if (service != null) {
-                            serviceNameTextView.setText(service.getName());
-                            ArrayAdapter<String> formFieldsAdapter = new ArrayAdapter<>(OfferedServicesActivity.this,
-                                    android.R.layout.simple_list_item_1, service.getFormFields());
-                            formFieldsListView.setAdapter(formFieldsAdapter);
-
-                            ArrayAdapter<String> docsFieldsAdapter = new ArrayAdapter<>(OfferedServicesActivity.this,
-                                    android.R.layout.simple_list_item_1, service.getDocsFields());
-                            docsFieldsListView.setAdapter(docsFieldsAdapter);
+                        if (dataSnapshot.exists() && dataSnapshot.hasChildren()) {
+                            DataSnapshot serviceSnapshot = dataSnapshot.getChildren().iterator().next();
+                            Service service = serviceSnapshot.getValue(Service.class);
+                            if (service != null) {
+                                showServiceDetailsDialog(service);
+                            } else {
+                                Toast.makeText(OfferedServicesActivity.this, "Service details not found.", Toast.LENGTH_LONG).show();
+                            }
+                        } else {
+                            Toast.makeText(OfferedServicesActivity.this, "Service not found in the database.", Toast.LENGTH_LONG).show();
                         }
                     }
 
                     @Override
                     public void onCancelled(DatabaseError databaseError) {
-                        Toast.makeText(OfferedServicesActivity.this, "Failed to load service details: " + databaseError.getMessage(), Toast.LENGTH_SHORT).show();
+                        Toast.makeText(OfferedServicesActivity.this, "Failed to load service details.", Toast.LENGTH_LONG).show();
                     }
                 });
-
-                builder.setNegativeButton("Close", (dialog, which) -> dialog.dismiss());
-                builder.show();
-                return true; // return true because the long click is handled
+                return true;
             }
         });
-
-
     }
 
+    private void loadOfferedServices(String branchId) {
+        branchesRef = FirebaseDatabase.getInstance().getReference("branches").child(branchId).child("servicesOffered");
+        branchesRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                offeredServiceNames.clear();
+                for (DataSnapshot serviceSnapshot : dataSnapshot.getChildren()) {
+                    String serviceName = serviceSnapshot.child("name").getValue(String.class);
+                    if (serviceName != null && !serviceName.trim().isEmpty()) {
+                        offeredServiceNames.add(serviceName);
+                    }
+                }
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Toast.makeText(OfferedServicesActivity.this, "Failed to load services.", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void showServiceDetailsDialog(Service service) {
+        // Create the dialog
+        AlertDialog.Builder builder = new AlertDialog.Builder(OfferedServicesActivity.this);
+        LayoutInflater inflater = getLayoutInflater();
+        View dialogView = inflater.inflate(R.layout.dialog_services, null);
+        builder.setView(dialogView);
+
+        // Set the service details in the dialog
+        TextView serviceNameTextView = dialogView.findViewById(R.id.ServiceNameDialogText);
+        ListView formFieldsListView = dialogView.findViewById(R.id.FormFieldDialogListView);
+        ListView docsFieldsListView = dialogView.findViewById(R.id.DocsFieldDialogListView);
+
+        serviceNameTextView.setText(service.getName());
+        ArrayAdapter<String> formFieldsAdapter = new ArrayAdapter<>(OfferedServicesActivity.this,
+                android.R.layout.simple_list_item_1, service.getFormFields());
+        formFieldsListView.setAdapter(formFieldsAdapter);
+
+        ArrayAdapter<String> docsFieldsAdapter = new ArrayAdapter<>(OfferedServicesActivity.this,
+                android.R.layout.simple_list_item_1, service.getDocsFields());
+        docsFieldsListView.setAdapter(docsFieldsAdapter);
+
+        // Show the dialog
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
 }
