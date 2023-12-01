dc4ddb82f93590bebe9c35b2a6ab4f89090e8829
ThilloAAG <145293301+ThilloAAG@users.noreply.github.com>
2023-12-01 16:31:43 -0500

    Update Client

--- app/src/main/java/com/example/servicenovigrad/Client.java
+++ app/src/main/java/com/example/servicenovigrad/Client.java
@@ -1,9 +1,29 @@
 package com.example.servicenovigrad;
 
+import java.util.List;
+
 public class Client extends Utilisateurs {
+
+    private String id;
+    private List<Service> requiredServices;
 
     public Client (String username, String password){
         super (username, password, "client");
-            
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public List<Service> getRequiredServices() {
+        return requiredServices;
+    }
+
+    public void setRequiredServices(List<Service> requiredServices) {
+        this.requiredServices = requiredServices;
     }
 }
