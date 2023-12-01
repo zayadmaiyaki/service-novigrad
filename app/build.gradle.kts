eb11eb4fb84c4b20ab0b7f140adef15317b759a8
ZayadMaiyaki <zmaiy092@uottawa.ca>
2023-12-01 16:19:29 -0500

    Service Novigrad Android Studio

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -71,9 +71,6 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     implementation ("com.google.android.material:material:1.8.0")
 
-    classpath("com.google.gms:google-services:4.5.0")
-    classpath("com.android.tools.build:gradle:4.2.1")
-
 
 
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
