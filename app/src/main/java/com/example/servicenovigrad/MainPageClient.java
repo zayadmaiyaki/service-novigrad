63e665fd6542ec2c9e1987a350913c953f5d82c9
Slaaayer420 <129106428+Slaaayer420@users.noreply.github.com>
2023-12-01 20:21:10 -0500

    RequestForServiceActivity and xml

--- app/src/main/java/com/example/servicenovigrad/MainPageClient.java
+++ app/src/main/java/com/example/servicenovigrad/MainPageClient.java
@@ -9,8 +9,18 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 public class MainPageClient extends AppCompatActivity {
 
@@ -18,6 +28,8 @@ public class MainPageClient extends AppCompatActivity {
     private TextView textView;
     private ListView ongoingRequestsListView;
     private Button newRequestButton;
+    private ArrayAdapter<String> adapter;
+    private ArrayList<String> ongoingRequests;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,9 +45,27 @@ public class MainPageClient extends AppCompatActivity {
         textClientPage.setText("Client Page");
         textView.setText("Ongoing Requests");
 
-        String[] ongoingRequests = {"Request 1", "Request 2", "Request 3"};
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, ongoingRequests);
+        ongoingRequests = new ArrayList<>();
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, ongoingRequests);
         ongoingRequestsListView.setAdapter(adapter);
+
+        DatabaseReference dbRef = FirebaseDatabase.getInstance().getReference("requestedservices");
+        dbRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                ongoingRequests.clear();
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    String serviceName = snapshot.getKey();
+                    ongoingRequests.add(serviceName);
+                }
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Toast.makeText(MainPageClient.this, "Failed to load requested services.", Toast.LENGTH_LONG).show();
+            }
+        });
 
         newRequestButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -49,7 +79,7 @@ public class MainPageClient extends AppCompatActivity {
             @Override
             public boolean onItemLongClick(AdapterView<?> adapterView, View view, int position, long id) {
                 // Show status
-                showLongClickDialog(ongoingRequests[position]);
+                showLongClickDialog(ongoingRequests.get(position));
                 return true;
             }
         });
@@ -75,3 +105,4 @@ public class MainPageClient extends AppCompatActivity {
         builder.create().show();
     }
 }
+
