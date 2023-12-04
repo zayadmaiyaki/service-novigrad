931d9e0919a7298d3d072289b5bf22aaa59ade52
Slaaayer420 <129106428+Slaaayer420@users.noreply.github.com>
2023-12-03 19:07:27 -0500

    RequestForServiceActivity and xml

---
+++ app/src/main/java/com/example/servicenovigrad/OfferedServicesForRequestActivity.java
@@ -0,0 +1,106 @@
+package com.example.servicenovigrad;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class OfferedServicesForRequestActivity extends AppCompatActivity {
+    private ListView selectServicesForRequestListView;
+    private ArrayAdapter<String> adapter;
+    private List<String> offeredServiceNames = new ArrayList<>();
+    private DatabaseReference branchesRef;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_offered_services_for_request);
+
+        selectServicesForRequestListView = findViewById(R.id.selectServiceForRequestListView);
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, offeredServiceNames);
+        selectServicesForRequestListView.setAdapter(adapter);
+
+        String branchId = getIntent().getStringExtra("BRANCH_ID");
+        if (branchId != null) {
+            loadOfferedServices(branchId);
+        } else {
+            Toast.makeText(this, "Branch ID not found.", Toast.LENGTH_LONG).show();
+            finish();
+        }
+
+        selectServicesForRequestListView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+
+                // Get the selected service name
+                String selectedServiceName = offeredServiceNames.get(position);
+
+                // Fetch the details for the selected service from Firebase
+                DatabaseReference serviceRef = FirebaseDatabase.getInstance().getReference("services");
+                serviceRef.orderByChild("name").equalTo(selectedServiceName).addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        if (dataSnapshot.exists() && dataSnapshot.hasChildren()) {
+                            DataSnapshot serviceSnapshot = dataSnapshot.getChildren().iterator().next();
+                            Service service = serviceSnapshot.getValue(Service.class);
+                            if (service != null) {
+                                // Redirect to a new page and pass the service ID
+                                Intent intent = new Intent(OfferedServicesForRequestActivity.this, RequestForServiceActivity.class);
+                                intent.putExtra("SERVICE_ID", service.getId());
+                                intent.putExtra("BRANCH_ID",branchId);
+                                startActivity(intent);
+                            } else {
+                                Toast.makeText(OfferedServicesForRequestActivity.this, "Service details not found.", Toast.LENGTH_LONG).show();
+                            }
+                        } else {
+                            Toast.makeText(OfferedServicesForRequestActivity.this, "Service not found in the database.", Toast.LENGTH_LONG).show();
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+                        Toast.makeText(OfferedServicesForRequestActivity.this, "Failed to load service details.", Toast.LENGTH_LONG).show();
+                    }
+                });
+                return true;
+            }
+        });
+
+    }
+
+    private void loadOfferedServices(String branchId) {
+        branchesRef = FirebaseDatabase.getInstance().getReference("branches").child(branchId).child("servicesOffered");
+        branchesRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                offeredServiceNames.clear();
+                for (DataSnapshot serviceSnapshot : dataSnapshot.getChildren()) {
+                    String serviceName = serviceSnapshot.child("name").getValue(String.class);
+                    if (serviceName != null && !serviceName.trim().isEmpty()) {
+                        offeredServiceNames.add(serviceName);
+                    }
+                }
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Toast.makeText(OfferedServicesForRequestActivity.this, "Failed to load services.", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
